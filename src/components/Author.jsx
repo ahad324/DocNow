05867f341d1f266204349d7af289bceb8f50a252
ahad <ahadg446@gmail.com>
2024-08-10 22:14:36 +0500

    Updated UI

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -3,15 +3,15 @@ import Logo from "../assets/Logo.jpg";
 
 const Author = () => {
   return (
-    <div className="fixed shadow-custom bottom-2 right-2 md:bottom-3 md:right-3 bg-gray-800 text-white text-xs p-2 rounded-lg z-10 flex items-center">
+    <div className="fixed bottom-1.5 right-1.5 z-10">
       <a
         href="https://ahad324.github.io/AllProjects/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center"
+        className="flex items-center w-12 bg-[--author-bg] text-[--author-text-color] text-xs p-2 rounded-lg border-2 border-[--author-text-color] transition-all duration-300 ease-in-out overflow-hidden whitespace-nowrap hover:w-48"
       >
-        <img src={Logo} alt="AbdulAhad" className="w-5 h-5 rounded-full" />
-        <span className="hidden sm:inline-block ml-2 underline cursor-pointer hover:text-gray-400">
+        <img src={Logo} alt="AbdulAhad" className="w-7 h-7 rounded-full" />
+        <span className="ml-3 font-semibold inline-block hover:underline">
           Made by AbdulAhad
         </span>
       </a>
